c4aee5d51498d170bdf5bda4a0c756ad08f8a4db
Tony Wang <t.wangzhimin@gmail.com>
2023-08-28 11:14:54 -0500

    feat: Add more eslint settings

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from './app';
 
-//decouple the app and the running server, so your tests can import the app without starting the server
+//decouple the app and the running server
+//, so your tests can import the app without starting the server
 
 const PORT = 9000;
 // Start the server
